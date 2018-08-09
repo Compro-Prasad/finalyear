581278130d7d13360164a4bcd649f656d31206a8
Compro-Prasad <comproprasad@gmail.com>
2018-08-09 11:27:43 +0530

    Some bug fixes in least cost

--- lpp/day2/least_cost.py
+++ lpp/day2/least_cost.py
@@ -1,4 +1,4 @@
-f#!/usr/bin/env python3
+#!/usr/bin/env python3
 
 import random
 
@@ -7,7 +7,6 @@ dem_sup_count = 3
 demand = []
 supply = []
 costs = []
-lc = []
 
 nr = int(input("Rows? "))
 nc = int(input("Cols? "))
@@ -15,49 +14,70 @@ nc = int(input("Cols? "))
 demand = input("Enter " + str(nc) + " demands: ")
 demand = list(map(int, demand.split()))
 
-supply = input("Enter " + str(nc) + " supplies: ")
+supply = input("Enter " + str(nr) + " supplies: ")
 supply = list(map(int, supply.split()))
 
 print("Enter costs:")
-for i in range(dem_sup_count):
+for i in range(nr):
     costs.append(list(map(int, input().split())))
-    lc.append(t)
 
 total_cost = 0
 
-while len(a) != 0:
-    min_cost = 0
+while len(costs) != 0:
+    min_cost = 100000000000000
     min_x = 0
     min_y = 0
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            if (min_cost < a[i][j]):
-                min_cost = a[i][j]
+    for i in range(len(costs)):
+        for j in range(len(costs[i])):
+            if (min_cost > costs[i][j]):
+                min_cost = costs[i][j]
                 min_x = j
                 min_y = i
     t1 = []  # costs
+    t2 = []  # demand
+    t3 = []  # supply
+    min_ = min(demand[min_x], supply[min_y])
+    demand[min_x] -= min_
+    supply[min_y] -= min_
+    #print(min_x, ", ", min_y)
+    print(demand)
+    #print(supply)
     if (demand[min_x] == supply[min_y]):
-        for i in range(len(a)):
+        for i in range(len(costs)):
             if (i != min_y):
                 temp = []
-                for j in range(len(a[i])):
+                for j in range(len(costs[i])):
                     if (j != min_x):
-                        temp.append(a[i][j])
+                        temp.append(costs[i][j])
+                        if (i != min_y and len(t2) == 0):
+                            t2.append(demand[j])
                 t1.append(temp)
+                t3.append(supply[i])
     elif (demand[min_x] < supply[min_y]):
-        for i in range(len(a)):
+        for i in range(len(costs)):
             temp = []
-            for j in range(len(a[i])):
+            for j in range(len(costs[i])):
                 if (j != min_x):
-                    temp.append(a[i][j])
+                    temp.append(costs[i][j])
+                    if (len(t2) == 0):
+                        t2.append(demand[j])
             if (len(temp) != 0):
                 t1.append(temp)
+                t3.append(supply[i])
     else:
-        for i in range(len(a)):
+        for i in range(len(costs)):
             if (i != min_y):
                 temp = []
-                for j in range(len(a[i])):
-                    temp.append(a[i][j])
+                for j in range(len(costs[i])):
+                    temp.append(costs[i][j])
+                    if (len(t2) == 0 and i != min_y):
+                        t2.append(demand[j])
                 t1.append(temp)
-    total_cost = min_cost * costs[i][j]
+                t3.append(supply[i])
+    total_cost += min_cost * costs[min_y][min_x]
     costs = t1
+    demand = t2
+    supply = t3
+    #print(costs)
+
+print("Total cost:", total_cost)
