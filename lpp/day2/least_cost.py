c142860e900957704c78d71f91239a8e7338cbcf
Compro-Prasad <comproprasad@gmail.com>
2018-08-15 11:38:36 +0530

    Some more bug fixes in least cost method

--- lpp/day2/least_cost.py
+++ lpp/day2/least_cost.py
@@ -17,63 +17,39 @@ demand = list(map(int, demand.split()))
 supply = input("Enter " + str(nr) + " supplies: ")
 supply = list(map(int, supply.split()))
 
+demand_count = len(demand)
+supply_count = len(supply)
+
 print("Enter costs:")
 for i in range(nr):
     costs.append(list(map(int, input().split())))
 
 total_cost = 0
 
-while len(costs) != 0:
+while demand_count > 0 and supply_count > 0:
     min_cost = 100000000000000
     min_x = 0
     min_y = 0
     for i in range(len(costs)):
         for j in range(len(costs[i])):
-            if (min_cost > costs[i][j]):
+            if (costs[i][j] > 0 and min_cost > costs[i][j]):
                 min_cost = costs[i][j]
                 min_x = j
                 min_y = i
-    t1 = []  # costs
-    t2 = []  # demand
-    t3 = []  # supply
     min_ = min(demand[min_x], supply[min_y])
     demand[min_x] -= min_
     supply[min_y] -= min_
     #print(min_x, ", ", min_y)
     print(demand)
     #print(supply)
-    if (demand[min_x] == supply[min_y]):
+    if (demand[min_x] == 0):
         for i in range(len(costs)):
-            if (i != min_y):
-                temp = []
-                for j in range(len(costs[i])):
-                    if (j != min_x):
-                        temp.append(costs[i][j])
-                        if (i != min_y and len(t2) == 0):
-                            t2.append(demand[j])
-                t1.append(temp)
-                t3.append(supply[i])
-    elif (demand[min_x] < supply[min_y]):
-        for i in range(len(costs)):
-            temp = []
-            for j in range(len(costs[i])):
-                if (j != min_x):
-                    temp.append(costs[i][j])
-                    if (len(t2) == 0):
-                        t2.append(demand[j])
-            if (len(temp) != 0):
-                t1.append(temp)
-                t3.append(supply[i])
-    else:
-        for i in range(len(costs)):
-            if (i != min_y):
-                temp = []
-                for j in range(len(costs[i])):
-                    temp.append(costs[i][j])
-                    if (len(t2) == 0 and i != min_y):
-                        t2.append(demand[j])
-                t1.append(temp)
-                t3.append(supply[i])
+            costs[i][min_x] = -1
+        demand_count -= 1
+    if (supply[min_y] == 0):
+        for i in range(len(costs[min_y])):
+            costs[min_y][i] = -1
+        supply_count -= 1
     total_cost += min_cost * costs[min_y][min_x]
     costs = t1
     demand = t2
