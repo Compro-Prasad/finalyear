c2c2ed7456a1d1c703091bc716cdcc124eb3c995
Compro-Prasad <comproprasad@gmail.com>
2018-09-11 22:50:49 +0530

    [Algo] Interval scheduling using weight and not greedily

--- algo/day3/interval-scheduling.cpp
+++ algo/day3/interval-scheduling.cpp
@@ -4,32 +4,68 @@
 
 using namespace std;
 
+struct Interval {
+  int start, finish;
+  int weight;
+  static bool compare(const Interval &x, const Interval &y) {
+    return x.finish < y.finish;
+  }
+};
+
+int get_last_valid_pos(vector<Interval> &x, int pos) {
+  for (int i = pos - 1; i >= 0; i--) {
+    if (x[i].finish <= x[pos].start)
+      return i;
+  }
+  return 0;
+}
+
+int find_max_in_interval(vector<Interval> &x, int pos) {
+  if (pos == 0) return x[pos].weight;
+
+  int with_current_weight = x[pos].weight;
+  int without_current_weight = find_max_in_interval(x, pos - 1);
+
+  int last_valid_pos = get_last_valid_pos(x, pos);
+  if (last_valid_pos)
+    with_current_weight += find_max_in_interval(x, last_valid_pos);
+
+  if (with_current_weight < without_current_weight)
+    cout << x[last_valid_pos].start << " " << x[last_valid_pos].finish
+         << ": " << x[last_valid_pos].weight << endl;
+  else
+    cout << x[pos].start << " " << x[pos].finish
+         << ": " << x[pos].weight << endl;
+
+  return max(with_current_weight, without_current_weight);
+}
+
+int find_max(vector<Interval> &x) {
+  if (x.size()) {
+    sort(x.begin(), x.end(), Interval::compare);
+    return find_max_in_interval(x, x.size() - 1);
+  }
+  return 0;
+}
+
 int main() {
   int lecture_count;
   cout << "Enter number of lectures: ";
   cin >> lecture_count;
   cout << "Enter lecture intervals: ";
-  vector<pair<int, pair<int, int>>> intervals(lecture_count);
+  vector<Interval> lecture_intervals(lecture_count);
   for (int i = 0; i < lecture_count; i++) {
-    int start, end, priority;
-    cin >> start >> end >> priority;
-    intervals[i] = make_pair(priority, make_pair(start, end));
+    int start, end, weight;
+    cin >> start >> end >> weight;
+    lecture_intervals[i] = Interval{start, end, weight};
   }
 
-  sort(intervals.rbegin(), intervals.rend());
-
-  int end = 0;
-
-  cout << "\nSelected lectures in the given class are:\n";
-
-  for (int i = 0; i < intervals.size(); i++) {
-
-    if (intervals[i].second.first >= end) {
-      end = intervals[i].second.second;
-      cout << intervals[i].second.first << ' '
-           << intervals[i].second.second << endl;
-    }
-
+  for (int i = 0; i < lecture_count; i++) {
+    Interval &x = lecture_intervals[i];
+    cout << x.start << " " << x.finish << ": " << x.weight << endl;
   }
+
+  cout << "Max profit: " << find_max(lecture_intervals) << endl;
+
   return 0;
 }
